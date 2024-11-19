96345cf51205089685b1db4370d5a9f9062ac188
jinsang park <jsgd1254@gmail.com>
2024-11-19 20:18:26 +0900

    init

---
+++ src/main/java/com/sparta/sample_currency/SampleCurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.sample_currency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleCurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleCurrencyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/sample_currency/SampleCurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.sample_currency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleCurrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
